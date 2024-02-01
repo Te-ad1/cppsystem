1fabf6f68d9c4627ff1b76ea12d161d6391b2cc6
Te-ad1 <129084495+Te-ad1@users.noreply.github.com>
2024-02-01 23:25:22 +0800

    Updateee test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,42 @@
 
-#include <windows.h>
+//#include <windows.h>
 #include <iostream>
-#include <unistd.h> 
+#include <unistd.h>
+#include <cstdlib>
+#include <stdlib.h> 
 using namespace std;
-HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+//HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 class pos{
 	public:
 	int choice;
 	int quantity;
 	int totalcost;
-	
-	int verification(){
+
+	    int animation(){
+	        cout << "                       Loading: ";
+
+            const char animationChars[] = { '|', '/', '-', '\\'};
+
+            const int numAnimationChars = sizeof(animationChars) / sizeof(animationChars[0]);
+            int animationIndex = 0;
+            const int numIterations = 50;
+
+            for (int i = 0; i < numIterations; ++i) {
+            std::cout << animationChars[animationIndex] << std::flush;
+            // Introduce a short delay for animation effect
+            usleep(100000); // usleep is in microseconds (1e6 microsec = 1 sec)
+            animationIndex = (animationIndex + 1) % numAnimationChars;
+            std::cout << '\b'; // Move the cursor back
+        }
+    
+            system ("CLS");
+	    }
+
+		
+		int verification(){
 		string name;
 		int password;
-		SetConsoleTextAttribute(h, 10);
+		//SetConsoleTextAttribute(h, 10);
 		cout << " 					==============================" << endl;
 		cout << "                                        Welcome to The Heritage Grill. " << endl;
 		cout << " 					============================= " << endl;
@@ -42,31 +65,31 @@ class pos{
 	
     int menu(){
 	//blue
-    SetConsoleTextAttribute(h, 2);
+    //SetConsoleTextAttribute(h, 2);
     cout << "                     ------[1]--------       ------[2]------       ------[3]------     ------[4]-------     \n";
     cout << "                     | Fried Chicken |       |    burger   |       |    Drinks   |     | French Fries |     \n";
     cout << "                     -----------------       ---------------       ---------------     ----------------     \n";
-    SetConsoleTextAttribute(h, 6);
+    //SetConsoleTextAttribute(h, 6);
     cout << "                     ------[5]-------       ------[6]------       ------[7]------      ------[8]------     \n";
     cout << "                     |    Snacks    |       |    Pizza    |       |    Tacos    |      |  Sandwiches |     \n";
     cout << "                     ----------------       ---------------       ---------------      ---------------      \n";
     
-    SetConsoleTextAttribute(h, 4);
+    //SetConsoleTextAttribute(h, 4);
     cout << "                                   ------[9]-------      ----[10]----    ----[0]----      \n";
     cout << "                                   |  Reservation |      |  Receipt |    |   Exit  |         \n";
     cout << "                                   ----------------      -----------     -----------   \n";
     
     
-    SetConsoleTextAttribute(h, 10);
+    //SetConsoleTextAttribute(h, 10);
     cout << "	Enter your choice: ";
 	cin >> choice;
-	SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
+	//SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
     return 0;		
 	}
 	
 
 	int transaction(){
-	    SetConsoleTextAttribute(h, 10);
+	    //SetConsoleTextAttribute(h, 10);
 		char transacloop;
 		char chickCategory;
 		
@@ -104,10 +127,10 @@ class pos{
 		cout << "Invalid choice!\n";
 		
 		break;
-		SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		//SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 		cout << endl;
-		SetConsoleTextAttribute(h, 10);
+		//SetConsoleTextAttribute(h, 10);
 		
 		
 
@@ -116,7 +139,7 @@ class pos{
 		cin >> transacloop;
 		}while(transacloop == 'y' || transacloop == 'y');
 		
-	   SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
+	   //SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
 	    return 0;	
 	
 	}
@@ -128,6 +151,7 @@ int main()
 {
     
     pos myObj;
+    myObj.animation();
     myObj.verification();
  
     myObj.menu();
