93cb1c16538575a11fd8f41c9a82f9364a0c7e94
Te-ad1 <129084495+Te-ad1@users.noreply.github.com>
2024-02-02 00:05:56 +0800

    adsdas test.cpp

--- test.cpp
+++ test.cpp
@@ -1,11 +1,12 @@
 
-//#include <windows.h>
+#include <windows.h>
 #include <iostream>
 #include <unistd.h>
 #include <cstdlib>
 #include <stdlib.h> 
+#include <ctime>
 using namespace std;
-//HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
 class pos{
 	public:
 	int choice;
@@ -13,7 +14,16 @@ class pos{
 	int totalcost;
 
 	    int animation(){
-	        cout << "                       Loading: ";
+	    	SetConsoleTextAttribute(h, 4);
+	    	cout << endl;
+	    	cout << endl;
+	    	cout << endl;
+	    	cout << endl;
+	    	cout << endl;
+	    	cout << endl;
+	    	cout << endl;
+	        cout << "                       	Loading ";
+	        
 
             const char animationChars[] = { '|', '/', '-', '\\'};
 
@@ -29,14 +39,17 @@ class pos{
             std::cout << '\b'; // Move the cursor back
         }
     
-            system ("CLS");
+         system("CLS"); 
+		 SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);  
 	    }
-
+		
 		
 		int verification(){
 		string name;
 		int password;
-		//SetConsoleTextAttribute(h, 10);
+		SetConsoleTextAttribute(h, 10);
+		cout << endl;
+	    cout << endl;
 		cout << " 					==============================" << endl;
 		cout << "                                        Welcome to The Heritage Grill. " << endl;
 		cout << " 					============================= " << endl;
@@ -52,11 +65,15 @@ class pos{
 		
 		if(name == "admin" && password == 123){
 			cout << "						Welcome admin!!" << endl;
+		
+		system ("CLS");
+		
 		}else{
+			
 			cout << "			Wrong username or password!!" << endl;
 			return 0;
 		}
-			
+		
 		cout << endl;
 		cout << endl;
 		
@@ -65,34 +82,40 @@ class pos{
 	
     int menu(){
 	//blue
-    //SetConsoleTextAttribute(h, 2);
+    SetConsoleTextAttribute(h, 2);
+    time_t now = time(0);
+    char* date_time = ctime(&now);
+
+    cout << "					  Current time: " << date_time << endl;
+    cout << endl;
     cout << "                     ------[1]--------       ------[2]------       ------[3]------     ------[4]-------     \n";
     cout << "                     | Fried Chicken |       |    burger   |       |    Drinks   |     | French Fries |     \n";
     cout << "                     -----------------       ---------------       ---------------     ----------------     \n";
-    //SetConsoleTextAttribute(h, 6);
+    SetConsoleTextAttribute(h, 6);
     cout << "                     ------[5]-------       ------[6]------       ------[7]------      ------[8]------     \n";
     cout << "                     |    Snacks    |       |    Pizza    |       |    Tacos    |      |  Sandwiches |     \n";
     cout << "                     ----------------       ---------------       ---------------      ---------------      \n";
     
-    //SetConsoleTextAttribute(h, 4);
+    SetConsoleTextAttribute(h, 4);
     cout << "                                   ------[9]-------      ----[10]----    ----[0]----      \n";
     cout << "                                   |  Reservation |      |  Receipt |    |   Exit  |         \n";
     cout << "                                   ----------------      -----------     -----------   \n";
     
     
-    //SetConsoleTextAttribute(h, 10);
+    SetConsoleTextAttribute(h, 10);
     cout << "	Enter your choice: ";
 	cin >> choice;
-	//SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
+	SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
     return 0;		
 	}
 	
 
 	int transaction(){
-	    //SetConsoleTextAttribute(h, 10);
+	    SetConsoleTextAttribute(h, 10);
 		char transacloop;
 		char chickCategory;
-		
+		char questdiscount;
+		int discount;
 		do{
 
 		switch(choice){
@@ -104,19 +127,47 @@ class pos{
 		cout << endl;
 	
 		cout << "		[a]Original Recipe Chicken (KFC)			55pesos" << endl;
-		cout << "		[b]Chicken McNuggets (McDonald's))			85pesos" << endl;
-		cout << "		[c]Chickenjoy (Jollibee))			        75pesos" << endl;
-		cout << "		[d]Spicy Chicken Sandwich (Chick-fil-A))	92pesos" << endl;
+		cout << "		[b]Chicken McNuggets (McDonald's)			85pesos" << endl;
+		cout << "		[c]Chickenjoy (Jollibee)			        75pesos" << endl;
+		cout << "		[d]Spicy Chicken Sandwich (Chick-fil-A) 	92pesos" << endl;
+		cout << endl;
 		cout << "	Enter your choice: ";
 		cin >> chickCategory;
 		
 		if(chickCategory == 'a'){
 		
 		cout << "		Choice: Original Recipe Chicken (KFC)			55pesos" << endl;
+		
 		cout << "\tEnter quantity: ";
 		cin >> quantity;
 		
-		totalcost = quantity * 55;
+		cout << "\tDo you have a discount:(Y/N): ";
+		cin >> questdiscount;
+			
+			if(questdiscount == 'y' || questdiscount == 'Y'){
+				cout << "	Type of discount: " <<endl;
+				cout << "		[1]Senior citizen Discount:  20% " << endl;
+				cout << "		[2]PWD Discount:  40% " << endl;
+				cout << endl;
+				cout << "	Enter your choice: " << endl;
+				cin >> discount;
+				
+				switch(discount){
+					case 1:
+						totalcost = quantity * 55 - (quantity * 55 * 0.20);
+					break;
+					
+					case 2:
+						totalcost = quantity * 55 - (quantity * 55 * 0.40);
+					break;
+					
+					default:
+						cout << "Pleas enter a valid discount!! " << endl;
+					break;
+				}
+			}	
+		
+		
 		
 		cout << "		Total cost: " <<totalcost;
 		
@@ -127,10 +178,10 @@ class pos{
 		cout << "Invalid choice!\n";
 		
 		break;
-		//SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+		SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
 		}
 		cout << endl;
-		//SetConsoleTextAttribute(h, 10);
+		SetConsoleTextAttribute(h, 10);
 		
 		
 
@@ -139,7 +190,7 @@ class pos{
 		cin >> transacloop;
 		}while(transacloop == 'y' || transacloop == 'y');
 		
-	   //SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
+	   SetConsoleTextAttribute(h, FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);	
 	    return 0;	
 	
 	}
